35dc8a21070ee6e8af456246c295553eccc8a860
Manoel Vieira <netinhovieira1801@gmail.com>
2023-11-07 22:51:27 -0300

    Global, Font e Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css'
+
+import logoSvg from '../assets/Logo.svg'
+
+export function Header(){
+  return (
+    <header className={styles.header}>
+      <img src={logoSvg} alt="Logotipo da aplicação" />
+    </header>
+  )
+}
