6aae85b9129b8650d1e17b8f083a25141ffcc2ee
Manoel Vieira <netinhovieira1801@gmail.com>
2023-11-08 22:17:34 -0300

    NewTaskForm

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,20 @@
 import styles from './Header.module.css'
 
 import logoSvg from '../../assets/Logo.svg'
+import { PlusCircle } from '@phosphor-icons/react/dist/ssr'
 
-export function Header(){
+export function Header() {
   return (
     <header className={styles.header}>
       <img src={logoSvg} alt="Logotipo da aplicação" />
+
+      <form className={styles.newTaskForm}>
+        <input placeholder='Adicione uma nova tarefa' />
+        <button>
+          Criar
+          <PlusCircle size={20}/>
+        </button>
+      </form>
     </header>
   )
 }
